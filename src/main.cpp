5bf4297ca43c784addb82682431602a290c12a7d
awbrown90 <aaronwbrown@msn.com>
2017-05-15 17:13:57 -0700

    feat: adding first commit for functioning MPC controller that can go at high speeds and be very cautious about going off the road. Latency also works but at slower speeds, will try to see if it can work at higher speeds as well.

--- src/main.cpp
+++ src/main.cpp
@@ -77,7 +77,7 @@ int main() {
     // The 4 signifies a websocket message
     // The 2 signifies a websocket event
     string sdata = string(data).substr(0, length);
-    cout << sdata << endl;
+    //cout << sdata << endl;
     if (sdata.size() > 2 && sdata[0] == '4' && sdata[1] == '2') {
       string s = hasData(sdata);
       if (s != "") {
@@ -92,20 +92,94 @@ int main() {
           double psi = j[1]["psi"];
           double v = j[1]["speed"];
 
+
+          for (int i = 0; i < ptsx.size(); i++ )
+          {
+          	
+          	//shift car reference angle to 90 degrees
+          	double shift_x = ptsx[i]-px;
+          	double shift_y = ptsy[i]-py;
+
+			ptsx[i] = (shift_x *cos(0-psi)-shift_y*sin(0-psi));
+			ptsy[i] = (shift_x *sin(0-psi)+shift_y*cos(0-psi));
+
+
+          }
+
+
+          double* ptrx = &ptsx[0];
+		  Eigen::Map<Eigen::VectorXd> ptsx_transform(ptrx, 6);
+
+		  double* ptry = &ptsy[0];
+		  Eigen::Map<Eigen::VectorXd> ptsy_transform(ptry, 6);
+
+          auto coeffs = polyfit(ptsx_transform, ptsy_transform, 3);
+
+          //caculate cte and epsi
+          double cte = polyeval(coeffs, 0);
+          double epsi = -atan(coeffs[1]);
+
+          Eigen::VectorXd state(6);
+          state << 0, 0, 0, v, cte, epsi;
+
           /*
           * TODO: Calculate steeering angle and throttle using MPC.
           *
           * Both are in between [-1, 1].
           *
           */
+
+          
+          auto vars = mpc.Solve(state, coeffs);
+
+
+          
+          vector<double> next_x_vals;
+          vector<double> next_y_vals;
+
+          
+          double poly_inc = 2.5;
+          int num_points = 25;
+          for (int i = 1; i < num_points; i++)
+          {
+          		next_x_vals.push_back(poly_inc*i);
+          		next_y_vals.push_back(polyeval(coeffs, poly_inc*i));
+          }
+
+
+          vector<double> mpc_x_vals;
+          vector<double> mpc_y_vals;
+
+          for (int i = 2; i < vars.size(); i++)
+          {
+          		if(i%2 == 0)
+          		{
+          			mpc_x_vals.push_back(vars[i]);
+          		}
+          		else
+          		{
+          			mpc_y_vals.push_back(vars[i]);
+          		}
+          }
+
           double steer_value;
           double throttle_value;
 
           json msgJson;
-          msgJson["steering_angle"] = steer_value;
-          msgJson["throttle"] = throttle_value;
+          msgJson["steering_angle"] = -vars[0];
+          msgJson["throttle"] = vars[1];
+
+          //msgJson["next_x"] = next_x.str();
+          //msgJson["next_y"] = next_y.str();
+
+          msgJson["next_x"] = next_x_vals;
+          msgJson["next_y"] = next_y_vals;
+
+          msgJson["mpc_x"] = mpc_x_vals;
+          msgJson["mpc_y"] = mpc_y_vals;
+
           auto msg = "42[\"steer\"," + msgJson.dump() + "]";
-          std::cout << msg << std::endl;
+          //std::cout << msg << std::endl;
           // Latency
           // The purpose is to mimic real driving conditions where
           // the car does actuate the commands instantly.
@@ -115,7 +189,7 @@ int main() {
           //
           // NOTE: REMEMBER TO SET THIS TO 100 MILLISECONDS BEFORE
           // SUBMITTING.
-          this_thread::sleep_for(chrono::milliseconds(100));
+          //this_thread::sleep_for(chrono::milliseconds(100));
           ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
         }
       } else {
